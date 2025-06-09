4a81654be8805e4d45ab5749f617ac4527e2bf58
likith <likithreddyn24@gmail.com>
2025-06-10 02:12:40 +0530

    first commit

---
+++ flipkart-app/src/main/java/com/mvit/App.java
@@ -0,0 +1,17 @@
+package com.mvit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int a=20;
+        int b=30;
+        int sum=a+b;
+        System.out.println( "Hello World!" );
+        System.out.println( "20+30 =" +sum );
+    }
+}
